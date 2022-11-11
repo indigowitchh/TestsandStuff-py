730261956082dc97f1c2996e772ef090109cbc72
indigowitchh <114239691+indigowitchh@users.noreply.github.com>
2022-11-11 07:47:54 -0700

    Create midtermattempt1.py

---
+++ midtermattempt1.py
@@ -0,0 +1,35 @@
+#---------------------------------------------------------------------------------------------------------------
+cookies=int(input("How many cookies do you have?:"))
+if cookies <= 3:
+    print("You dont have enough cookies :(")
+elif cookies <= 10:
+    print("You have a good amount of cookies!")
+else:
+    print("OMG you have a lot, give me some?")
+#---------------------------------------------------------------------------------------------------------------
+lightsaber=input("Are you a jedi or a sith?")
+if lightsaber == 'Jedi' or lightsaber == 'jedi' or lightsaber == 'j':
+    print("You get a green light saber!")
+elif lightsaber == 'Sith' or lightsaber == 'sith' or lightsaber == 's':
+    print("You get a red lightsaber!")
+else:
+    print("You get a breadstick :(")
+#---------------------------------------------------------------------------------------------------------------
+for k in range (4, 41, 2):
+    print(k, end = " ")
+#---------------------------------------------------------------------------------------------------------------
+choice = input
+'q'  == True
+while choice != 'q':
+    for j in range(100, 40, -10):
+        print(j, end = " ")
+while choice == 'q':
+        'q' == False
+#---------------------------------------------------------------------------------------------------------------
+joke = input("Knock knock! whos there?:")
+while input != 'orange':
+    print("banana!")
+    print(joke, end = " ")
+while input == 'orange':
+    print("orange you glad i said banana?!")
+#--------------------------------------------------------------------------------------------------------------
