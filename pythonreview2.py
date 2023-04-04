ec44f8c7d5893a293e012976ab75722a916dbd98
rey <114239691+indigowitchh@users.noreply.github.com>
2023-04-04 09:57:22 -0600

    Create pythonreview2.py

---
+++ pythonreview2.py
@@ -0,0 +1,7 @@
+#function while loop-----------------------------------
+#RANK: 3
+def GiveCompliment(name):
+    print(name, "you look so nice today!")
+while True:
+    name = input("What is your name:")
+    GiveCompliment(name)
