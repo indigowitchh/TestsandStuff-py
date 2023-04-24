c9786d3573ac73f151ac80d0830bf1e4f35aa10e
rey <114239691+indigowitchh@users.noreply.github.com>
2023-04-24 09:43:54 -0600

    Create functionreview.py

---
+++ functionreview.py
@@ -0,0 +1,14 @@
+def FirstBigger(num1 , num2):
+    if num1 > num2:
+        return True
+    else:
+        return False
+    
+    
+print(FirstBigger(6, 15))
+
+def Badger():
+    for i in range(10):
+        print("Badger!")
+
+Badger()
